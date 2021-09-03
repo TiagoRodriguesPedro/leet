9f86641116afcb213b6fe5fd66918e5b6755f20c
Tiago Rodrigues <isatiago098@gmail.com>
2021-09-03 18:05:44 -0300

    resolução exerciçio leet

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.Write("texto a ser traduzido: ");
+          Console.WriteLine(
+           Console.ReadLine()
+
+
+            .Replace("A","4")
+             .Replace("E", "3")
+             .Replace("e", "3")
+             .Replace("I","1")
+               .Replace("i","1")
+                .Replace("O","0")
+             .Replace("o","0")
+            .Replace("t","7")
+              .Replace("T","7")
+              .Replace("S","5")
+        
+              .Replace("s","5")
+              .Replace("j","6")
+ );
+              }
+
+        }
+    }
+
