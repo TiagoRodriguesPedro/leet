399bdd317f9317c389be2976d26fb9a53e0f68fd
Diego Neri <diego@diegoneri.com>
2021-09-17 20:09:32 -0300

    Corrige indentação

--- Program.cs
+++ Program.cs
@@ -6,27 +6,26 @@ namespace leet
     {
         static void Main(string[] args)
         {
-         Console.Write("texto a ser traduzido: ");
-          Console.WriteLine(
-           Console.ReadLine()
+            Console.Write("texto a ser traduzido: ");
+            Console.WriteLine(
+                Console.ReadLine()
 
+                .Replace("A", "4")
+                .Replace("E", "3")
+                .Replace("e", "3")
+                .Replace("I", "1")
+                .Replace("i", "1")
+                .Replace("O", "0")
+                .Replace("o", "0")
+                .Replace("t", "7")
+                .Replace("T", "7")
+                .Replace("S", "5")
 
-            .Replace("A","4")
-             .Replace("E", "3")
-             .Replace("e", "3")
-             .Replace("I","1")
-               .Replace("i","1")
-                .Replace("O","0")
-             .Replace("o","0")
-            .Replace("t","7")
-              .Replace("T","7")
-              .Replace("S","5")
-        
-              .Replace("s","5")
-              .Replace("j","6")
- );
-              }
+                .Replace("s", "5")
+                .Replace("j", "6")
+            );
+         }
 
-        }
-    }
+     }
+}
 
